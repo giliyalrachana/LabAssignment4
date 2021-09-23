2e667f606eda01d79d80f53dbd08f66657ebb28d
Rachana <giliyalrachana@gmail.com>
2021-09-23 13:21:41 +0530

    LabAssignment4
    New Classes are added

---
+++ LabAssignment4/LabAssignment4/ElementSearchInJaggedArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LabAssignment4
+{
+    class ElementSearchInJaggedArray
+    {
+        static void Main()
+        {
+            int[][] jaggedArray = new int[2][];
+            jaggedArray[0] = new int[] { 1, 2, 3 };
+            jaggedArray[1] = new int[] { 4, 5 };
+            for (int i = 0; i < 2; i++)
+                Convert.ToInt32(
+            {
+                Console.Write($"{ i[0, 1]}\t");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+}
+
+
+
+
+
+
+
---
+++ LabAssignment4/LabAssignment4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LabAssignment4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = { 5,6,7,8,9,10 };
+           //  numsArray[0] = 11;
+            Console.WriteLine($"numsArray[1]={numsArray[1]}");
+
+            foreach (var num in numsArray)
+            {
+                Console.Write($"{num}\t");
+            }
+            Console.ReadLine();
+        }
+    }
+}
